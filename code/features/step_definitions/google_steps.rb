6d55bd8b9d69b1b316a0ad55f1f4705e1c5d1435
Alex von Renteln <alex@aeffle.de>
2016-11-17 22:34:33 +0100

    new rspec syntax

--- code/features/step_definitions/google_steps.rb
+++ code/features/step_definitions/google_steps.rb
@@ -8,13 +8,12 @@ When /^I fill in "([^"]*)" with "([^"]*)"$/ do |element, text|
 end
 
 Then(/^I should see "(.*?)"$/) do |text|
-  page.should have_content text
+  expect(page).to have_content text
   #page.driver.resize(20,30)
   #page.save_screenshot("/path/to/test.pdf")
   #puts page.within_window
   #puts page.driver.network_traffic
   #puts page.driver.cookies
   #puts page.response_headers.to_a
-
 end
 
